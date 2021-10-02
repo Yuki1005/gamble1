74637ee6ffc4a4ddda9c5d2b1f64aa444864b46d
Yuki1005 <yhiramatsu5@icloud.com>
2021-10-02 23:41:00 +0900

    1

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
 #少なくとも指定のキャラクターが1回以上出るガチャ確立プログラム
-print("キャラクターの排出率を入力してください(％)")
-percent = float(input("キャラクターの排出率を入力してください(％)\n")) / 100
-print('{:%}'.format(percent))
+print("排出率は？(％)：")
+percent = float(input())
 
-print("ガチャを引く回数を入力してください(回)")
-times = input()
-print(times + "回")
+print("引く回数は？(回)：")
+times = float(input())
 
 print("少なくとも１回以上出る確率は....")
-answer = 1 - ((1 - percent) ** float(times))
-print('{:%}'.format(answer))
+answer = 1 - ((1 - percent / 100) ** float(times))
+print("約" , int(answer*100) , "％です" , sep = "")
